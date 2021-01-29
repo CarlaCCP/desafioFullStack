1f329729b65b77d97b508e6e4000c7b37f24f4d4
Carla Cristina <carldepaula230@gmail.com>
2021-01-29 17:38:52 -0300

    wip

--- desafioFullStack/src/main/java/repository/RepositoryUsuario.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Usuario;
-
-public interface RepositoryUsuario extends JpaRepository<Usuario, Long> {
-
-}
